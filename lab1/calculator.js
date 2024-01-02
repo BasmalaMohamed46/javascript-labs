ba0cc4678d173fd7cb172b65cddd046bd6e13abf
Basmala Mohamed Zain El Abidine <102691187+BasmalaMohamed46@users.noreply.github.com>
2024-01-02 12:53:19 +0200

    add lab1

---
+++ lab1/calculator.js
@@ -0,0 +1,5 @@
+alert(" it's the first release of a calculator that only has a summation feature.")
+var firstNumber=prompt("Enter your first number");
+var secondNumber=prompt("Enter your second number");
+var summation=Number(firstNumber)+Number(secondNumber);
+alert("The summation of " + firstNumber + '+' + secondNumber+'=' +" " + summation)
---
+++ lab1/welcome.js
@@ -0,0 +1,9 @@
+var firstName=prompt("Enter your first name");
+var lastName=prompt("Enter your last name");
+
+var confirmFullName=confirm("Are you sure your name is" + " " + firstName + " " + lastName);
+
+var birthYear=prompt("Enter your birth year");
+var age=2023-birthYear;
+
+alert("welcome" + " " + firstName + " " + lastName + " " + "you are" + " " + age + " " + "years old")
